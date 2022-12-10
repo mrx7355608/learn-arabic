2a78d47476126c7aa6a59f62b32a9ecc19e9ffba
mrx7355608 <mrx7355608@gmail.com>
2022-12-10 20:54:09 +0500

    added get lecture by id route

--- src/api/lectures/lectures.routes.js
+++ src/api/lectures/lectures.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const asyncErrorHandler = require("express-async-handler");
 const lectureDataValidator = require("@middlewares/lectureDataValidator");
 const LectureServices = require("./lectures.services");
 
@@ -10,6 +11,13 @@ router.get("/", async (req, res, next) => {
     const { results, data } = await services.fetchData();
     return res.status(200).json({ results, data });
 });
+router.get(
+    "/:id",
+    asyncErrorHandler(async (req, res, next) => {
+        const { lecture } = await services.fetchById(req.params.id);
+        return res.status(200).json({ lecture });
+    })
+);
 router.post("/create", lectureDataValidator, async (req, res, next) => {
     const { newLecture } = await services.create(req.body);
     return res.status(201).json({ newLecture });
