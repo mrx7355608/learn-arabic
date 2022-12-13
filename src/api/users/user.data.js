1dc228c682cbf429b8b894630fc2fae923064043
mrx7355608 <mrx7355608@gmail.com>
2022-12-13 11:28:57 +0500

    created user model and data access layer

---
+++ src/api/users/user.data.js
@@ -0,0 +1,39 @@
+const UserModel = require("./user.model");
+
+exports.getUser = async (id) => {
+    const user = await UserModel.findById(id);
+    return user;
+};
+exports.getUserByEmail = async (email) => {
+    const user = await UserModel.findOne({ email });
+    return user;
+};
+exports.createUser = async (userData) => {
+    const newUser = await UserModel.create({ ...userData, role: "user" });
+    return newUser;
+};
+exports.updateUser = async (data) => {
+    const updatedUserData = await UserModel.findByIdAndUpdate(id, data, {
+        new: true,
+        $projection: {
+            email: false,
+            password: false,
+            isEmailVerified: false,
+            isAccountActive: false,
+        },
+    });
+    return updatedUserData;
+};
+exports.deleteUser = async (id) => {
+    const deletedUser = await UserModel.findByIdAndDelete(id);
+    return deletedUser;
+};
+exports.verifyEmail = async (id) => {
+    // TODO: return true or false based on the update success
+    const verified = await UserModel.findByIdAndUpdate(id, {
+        isEmailVerified: true,
+    });
+    return verified;
+};
+exports.resetPassword = async (id) => {};
+exports.forgotPassword = async (id) => {};
---
+++ src/api/users/user.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    lname: {
+        type: String,
+        required: true,
+        min: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false,
+        select: false,
+    },
+    // Used to check that if account is deleted or not
+    isAccountActive: {
+        type: Boolean,
+        default: true,
+        select: false,
+    },
+});
+
+// Hash password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+    this.password = hashedPassword;
+    next();
+});
+// Assign random profile pictures
+
+const UserModel = mongoose.model("User", userSchema);
