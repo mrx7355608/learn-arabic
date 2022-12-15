59a41735611bb151760ae10023678076eea833f6
mrx7355608 <mrx7355608@gmail.com>
2022-12-15 08:55:12 +0500

    handling passportjs errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -11,6 +11,13 @@ module.exports = (err, req, res, next) => {
         return res.status(400).json({ error: "Invalid Id" });
     if (err.name === "ValidationError")
         return res.status(400).json({ error: err.message });
+    if (err.name === "AuthenticationError") {
+        const message =
+            err.message === "Unauthorized"
+                ? "Incorrect email or password"
+                : "Invalid data";
+        return res.status(400).json({ error: message });
+    }
 
     return res.status(code).json({ error: err.message });
 };
