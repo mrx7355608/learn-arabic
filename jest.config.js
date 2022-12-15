3420790e44327c446d4f770c29ab7c8fa057bac8
mrx7355608 <mrx7355608@gmail.com>
2022-12-15 10:02:50 +0500

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    moduleNameMapper: {
+        "^@config(.*)$": "<rootDir>/src/config/$1",
+        "^@middlewares(.*)$": "<rootDir>/src/middlewares/$1",
+        "^@utils(.*)$": "<rootDir>/src/utils/$1",
+        "^@api(.*)$": "<rootDir>/src/api/$1",
+    },
+};
