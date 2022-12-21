5ea81235a4290da60081ed7c04924517529b8e8c
mrx7355608 <mrx7355608@gmail.com>
2022-12-21 11:47:04 +0500

    refactored and added ResetPassword emails

--- src/utils/Email.js
+++ src/utils/Email.js
@@ -15,26 +15,24 @@ class Email {
         return transport;
     }
 
-    prepareEmail(recipient_email, userid) {
-        const token = createToken(userid);
-        const message = createEmailVerificationMessage(recipient_email, token);
-        return message;
-    }
-
-    async send(user_email, userid) {
+    async send(message) {
         const transport = this.setupTransport();
-        const message = this.prepareEmail(user_email, userid);
         const response = await transport.sendMail(message);
         return response;
     }
 }
 
-const createEmailVerificationMessage = (recipient_email, token) => {
-    return {
-        from: config.MAIL_SENDER,
-        to: recipient_email,
-        subject: "Email Verification",
-        html: `
+class ResetPasswordEmail extends Email {
+    constructor() {
+        super();
+    }
+
+    createMessage(recipient_email, token) {
+        const message = {
+            from: config.MAIL_SENDER,
+            to: recipient_email,
+            subject: "Reset Password",
+            html: `
             <div style="padding: 10px" >
                 <p style="font-size:20px" >Thank you for signing up</p>
                 <p>Please verify your email below</p>
@@ -55,7 +53,57 @@ const createEmailVerificationMessage = (recipient_email, token) => {
                 </a>
             </div>
     `,
-    };
-};
+        };
+        return message;
+    }
 
-module.exports = Email;
+    async sendEmail(userid, recipient_email) {
+        const token = createToken(userid);
+        const message = this.createMessage(recipient_email, token);
+        await this.send(message);
+    }
+}
+
+class AccountVerificationEmail extends Email {
+    constructor() {
+        super();
+    }
+
+    createMessage(recipient_email, token) {
+        const message = {
+            from: config.MAIL_SENDER,
+            to: recipient_email,
+            subject: "Email Verification",
+            html: `
+            <div style="padding: 10px" >
+                <p style="font-size:20px" >Thank you for signing up</p>
+                <p>Please verify your email below</p>
+                <a 
+                    style="
+                    border-radius: 1000px;
+                    background: #2d2d2d;
+                    color: white;
+                    outline: none;
+                    border: none;
+                    font-weight: 500;
+                    font-size: 13px;
+                    padding: 9px 15px;
+                    text-decoration: none;
+                    "
+                    href="http://localhost:8000/api/v1/auth/verify-email?token=${token}" >
+                    Verify Email
+                </a>
+            </div>
+    `,
+        };
+        return message;
+    }
+
+    async sendEmail(recipient_email, userid) {
+        const token = createToken(userid);
+        const message = this.createMessage(recipient_email, token);
+        this.send(message);
+    }
+}
+
+module.exports = { AccountVerificationEmail, ResetPasswordEmail };
